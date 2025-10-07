611857136505f4a37ee03e1524b2b089cf5cb858
MauroIgnacioTagliaferri <mauro89_2@hotmail.com>
2025-10-07 19:26:04 -0300

    Logo y spinner ordenados

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -3,18 +3,15 @@ import WSMS2Logo from "@/app/ui/WSMS2Logo";
 export default function LoadingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <WSMS2Logo className="mb-10" />
-      <div className="flex items-center justify-center align-center h-15 shrink-0 rounded-lg p-4 md:h-20">
-              <div className="flex items-center justify-center mt- gap-x-0">
-                            
-                            <WSMS2Logo className="h-20 w-20 ml-3" />
+      {/* Logo centrado */}
+      <WSMS2Logo className="h-60 w-60 mb-10" />
 
-                          </div>
-            </div>
-      <div className="spinner">
+      {/* Spinner debajo del logo */}
+      <div className="spinner mt-8">
         <div></div><div></div><div></div><div></div>
         <div></div><div></div><div></div><div></div>
       </div>
     </div>
   );
 }
+
