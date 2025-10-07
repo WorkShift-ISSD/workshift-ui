77af016cbefeae7d0170af58550463dece72832f
Sofia DG <m.sofia.dg@hotmail.com>
2025-10-07 19:10:23 -0300

    logo spiner

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -1,9 +1,16 @@
-import WSMSLogo from "@/app/ui/WSMSLogo";
+import WSMS2Logo from "@/app/ui/WSMS2Logo";
 
 export default function LoadingPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-white">
-      <WSMSLogo className="mb-10" />
+      <WSMS2Logo className="mb-10" />
+      <div className="flex items-center justify-center align-center h-15 shrink-0 rounded-lg p-4 md:h-20">
+              <div className="flex items-center justify-center mt- gap-x-0">
+                            
+                            <WSMS2Logo className="h-20 w-20 ml-3" />
+
+                          </div>
+            </div>
       <div className="spinner">
         <div></div><div></div><div></div><div></div>
         <div></div><div></div><div></div><div></div>
--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,21 @@
-import AcmeLogo from '@/app/ui/MigraLogo';
+
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import Image from 'next/image';
 import { inter } from './ui/fonts';
 import LoginForm from './components/LoginForm';
 import MigraLogo from '@/app/ui/MigraLogo';
+import WSMSLogo from '@/app/ui/WSMSLogo';
 
 export default function Page() {
   return (
-    <main className="flex min-h-screen flex-col p-6">
+    <main className="flex min-h-screen flex-col p-3">
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-cddcea p-4 md:h-40"
       style={{ backgroundColor: '#cddcea' }}>
         <MigraLogo className=" relative w-[30%] h-[80px] md:h-[120px]" />
       </div>
-      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
-        <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-1 md:w-2/5 md:px-20">
+      <div className=" flex grow flex-col gap-4 md:flex-row mt-2">
+        <div className="flex flex-col justify-center gap-2 rounded-lg bg-gray-50 md:w-2/5 md:px-10 ">
           <p className={`${inter.className} antialiased text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <div/>
             <strong>Bienvenido </strong> al sistema de {' '}
@@ -29,6 +30,15 @@ export default function Page() {
           <LoginForm/>
         </div>
       </div>
+    <div className="flex items-center justify-center align-center h-15 shrink-0 rounded-lg p-4 md:h-20"
+      style={{ backgroundColor: '#cddcea' }}>
+        <div className="flex items-center justify-center mt- gap-x-0">
+                      <span className="text-sm text-gray-700 flex  items-center">Powered by
+                      <WSMSLogo className="h-20 w-20 ml-3" />
+                      </span>
+                    </div>
+      </div>
+
     </main>
   );
 }
