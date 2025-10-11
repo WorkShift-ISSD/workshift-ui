be3fff683a6eeaa4cf742486b9d47aaabef3de5f
MauroIgnacioTagliaferri <mauro89_2@hotmail.com>
2025-10-11 11:29:58 -0300

    modo dark loading

--- app/loading/page.tsx
+++ app/loading/page.tsx
@@ -2,16 +2,22 @@ import WSMS2Logo from "@/app/ui/WSMS2Logo";
 
 export default function LoadingPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-white">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white dark:bg-gray-900">
       {/* Logo centrado */}
       <WSMS2Logo className="h-60 w-60 mb-10" />
 
       {/* Spinner debajo del logo */}
       <div className="spinner mt-8">
-        <div></div><div></div><div></div><div></div>
-        <div></div><div></div><div></div><div></div>
+        {/* Usamos Tailwind para animación y color según modo */}
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
+        <div className="bg-blue-600 dark:bg-blue-400"></div>
       </div>
     </div>
   );
 }
-
