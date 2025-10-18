a7cde89a166689e9268489ba5be35b1fb95c0766
quique <enriquechavez1982@hotmail.com>
2025-10-18 11:56:22 -0300

    changes in oferta

--- app/dashboard/cambios/page.tsx
+++ app/dashboard/cambios/page.tsx
@@ -1,9 +1,10 @@
+import OfertaCambios from "@/app/oferta/OfertaCambios";
+
 export default function CambiosPage() {
   return (
     <div className="p-6">
       <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-gray-100">Cambios de Turno</h1>
-      <p className="text-gray-700 dark:text-gray-300">Aca irian los cambios</p>
-      <img src="/EnConstrucción(2).png" alt="En Construcción" />
+      <OfertaCambios/>
     </div>
   );
 }
